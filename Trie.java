afabe2f8c085223f99a098d8cbb8f1a6083bca91
sushantkp <sushantkp@yahoo.com>
2014-06-21 12:11:06 -0700

    Working trie. checks if string present in index

--- Trie.java
+++ Trie.java
@@ -2,24 +2,25 @@ import java.util.UUID;
 import java.util.List;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.TreeMap;
 
 
 // implements prefix free trie
 public class Trie {
-    private static final char SENTINEL = 0d;
+    private static final char SENTINEL = 0;
     private Node root = new InnerNode(); 
     
     public void insert(String s) {
         checknull(s);
         Node t = root;
         int j = 0;
-        while (j < s.size() && t.child(s.charAt(j) != null) {
+        while (j < s.length() && t.child(s.charAt(j)) != null) {
             t = t.child(s.charAt(j)); 
             j++;
         }   
-        while (j < s.size()) {
+        while (j < s.length()) {
             Node u = new InnerNode();
-            t.addChild(s.charAt(j);
+            t.addChild(s.charAt(j), u);
             t = u;
             j++;
         }
@@ -32,11 +33,11 @@ public class Trie {
         checknull(s);
         Node t = root;
         int j = 0;
-        while (j < s.size() && t.child(s.charAt(j) != null) {
+        while (j < s.length() && t.child(s.charAt(j)) != null) {
             t = t.child(s.charAt(j));
             j++;
         }
-        if (j == s.size()) {
+        if (j == s.length()) {
             return t.child(SENTINEL) != null;   
         }
         return false;
@@ -44,22 +45,22 @@ public class Trie {
 
 
     void checknull(String s) {
-        if (s == null s.isEmpty() {
-            throw new NullPointerException("String null or empty");
+        if (s == null) {
+            throw new NullPointerException("String input null");
         }
     }
 
     private interface Node {
         Node child(char c);
-        void addchild(char c, Node t);
+        void addChild(char c, Node t);
     }
 
     private class InnerNode implements Node {
         final Map<Character, Node> childMap = new TreeMap<Character, Node>(); // check sync needed
-        Node child(char c) {
+        public Node child(char c) {
             return childMap.get(c);
         } 
-        void addChild(char c, Node t) {
+        public void addChild(char c, Node t) {
             childMap.put(c, t);
         } 
 
@@ -67,8 +68,35 @@ public class Trie {
 
     private class Sentinel implements Node {
         final List<UUID> docids = new LinkedList();        
-        void add(UUID docid) {
+        void adddoc(UUID docid) {
             docids.add(docid);
         }
+        public Node child(char c) {
+            throw new UnsupportedOperationException();
+        }
+        public void addChild(char c, Node t) {
+            throw new UnsupportedOperationException();
+        }
     }
+
+
+    public static void main(String [] args) {
+        String[] doc = new String [] {"pot", "potato", "pottery", "tempo", "tattoo"};
+        Trie t = new Trie();
+
+        for (String s : doc) {
+            t.insert(s);
+        }
+
+        System.out.println(t.lookup("tempo"));
+        System.out.println(t.lookup("trie"));
+        System.out.println(t.lookup(""));
+        try {
+            System.out.println(t.lookup(null));
+            assert false;
+        }
+        catch (NullPointerException e) {
+
+        }
+    } 
 }
